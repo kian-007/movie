9e562e381feed3263da889f9cd6f694ce305039e
kian-007 <79346418+kian-007@users.noreply.github.com>
2022-02-06 13:18:26 +0330

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ include_once 'lib/module.php';
 
 define('SITE_URL', 'http://localhost/php-movie/');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'dbpython');
-define('DB_PORT', 5432);
+define('DB_NAME', 'blablabla');
+define('DB_PORT', ???);
 define('DB_USER', 'postgres');
-define('DB_PASSWORD', 'king.kian007');
+define('DB_PASSWORD', 'blablabla');
 
 function home_url($path = null){
     if(!$path || $path == '/'){
